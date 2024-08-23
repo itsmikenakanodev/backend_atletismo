80557bec03525262da2a6cde12ff8e0faa8b441a
itsmikenakanodev <mike.dev.programacion@gmail.com>
2024-08-23 12:31:32 -0500

    Correccion dependencia ciclica de campeonato y prueba con campeonatospruebas

--- src/main/java/com/atletismo/Repository/Modelo/Prueba.java
+++ src/main/java/com/atletismo/Repository/Modelo/Prueba.java
@@ -1,5 +1,6 @@
 package com.atletismo.Repository.Modelo;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -30,9 +31,11 @@ public class Prueba {
     private String descripcion;
 
     @OneToMany(mappedBy = "prueba")
+    @JsonIgnore
     private List<CampeonatoPrueba> campeonatoPruebas;
 
     @OneToMany(mappedBy = "prueba")
+    @JsonIgnore
     private List<Resultado> resultados;
 
 }

cd66c4c154826239525fc8c03de96794faeebdd8
itsmikenakanodev <mike.dev.programacion@gmail.com>
2024-11-09 10:37:15 -0500

    Correccion de conteo de los reportes de campeonato

--- src/main/java/com/atletismo/Repository/ReportesRepositoryImpl.java
+++ src/main/java/com/atletismo/Repository/ReportesRepositoryImpl.java
@@ -19,12 +19,12 @@ public class ReportesRepositoryImpl implements IReportesRepository{
     public CampeonatoCompetidorCountDTO contarCompetidoresPorCampeonato(Integer idCampeonato) {
         String sql = "SELECT new com.atletismo.Service.dto.CampeonatoCompetidorCountDTO(" +
                 "c.nombre, " +
-                "SUM(CASE WHEN u.sexo = 'M' THEN 1 ELSE 0 END), " +
-                "SUM(CASE WHEN u.sexo = 'F' THEN 1 ELSE 0 END)) " +
+                "SUM(CASE WHEN u.sexo = 'M' AND comp.estadoParticipacion = 'Confirmado' THEN 1 ELSE 0 END), " +
+                "SUM(CASE WHEN u.sexo = 'F' AND comp.estadoParticipacion = 'Confirmado' THEN 1 ELSE 0 END)) " +
                 "FROM Campeonato c " +
                 "JOIN Competidor comp ON comp.campeonato.id = c.id " +
                 "JOIN Usuario u ON comp.usuario.id = u.id " +
-                "WHERE comp.estadoParticipacion = 'Pendiente' AND c.id= :campeonatoId " +
+                "WHERE c.id= :campeonatoId " +
                 "GROUP BY c.nombre " +
                 "ORDER BY c.nombre";
 
@@ -37,14 +37,15 @@ public class ReportesRepositoryImpl implements IReportesRepository{
     public List<EventoCompetidorCountDTO> contarCompetidoresPorEvento(Integer idCampeonato) {
         String sql = "SELECT new com.atletismo.Service.dto.EventoCompetidorCountDTO(" +
                 "e.nombre, " +
-                "COUNT(DISTINCT CASE WHEN u.sexo = 'M' THEN comp.id ELSE NULL END), " +
-                "COUNT(DISTINCT CASE WHEN u.sexo = 'F' THEN comp.id ELSE NULL END)) " +
+                "COUNT(DISTINCT CASE WHEN u.sexo = 'M' AND comp.estadoParticipacion = 'Confirmado' THEN comp.id ELSE NULL END), " +
+                "COUNT(DISTINCT CASE WHEN u.sexo = 'F' AND comp.estadoParticipacion = 'Confirmado' THEN comp.id ELSE NULL END)) " +
                 "FROM Campeonato c " +
-                "JOIN Competidor comp ON comp.campeonato.id = c.id " +
-                "JOIN Usuario u ON comp.usuario.id = u.id " +
-                "JOIN Resultado cr ON comp.id = cr.competidor.id " +
-                "JOIN Prueba e ON cr.prueba.id = e.id " +
-                "WHERE comp.estadoParticipacion = 'Pendiente' AND c.id = :campeonatoId " +
+                "JOIN CampeonatoPrueba cp ON cp.campeonato.id = c.id "+
+                "JOIN Prueba e ON cp.prueba.id = e.id "+
+                "LEFT JOIN Resultado cr ON cr.prueba.id = e.id "+
+                "LEFT JOIN Competidor comp ON comp.id = cr.competidor.id "+
+                "LEFT JOIN Usuario u ON comp.usuario.id = u.id "+
+                "WHERE c.id = :campeonatoId "+
                 "GROUP BY c.nombre, e.nombre " +
                 "ORDER BY c.nombre, e.nombre";
 
@@ -68,7 +69,7 @@ public class ReportesRepositoryImpl implements IReportesRepository{
                 "JOIN Usuario u ON comp.usuario.id = u.id " +
                 "JOIN Resultado cr ON comp.id = cr.competidor.id " +
                 "JOIN Prueba e ON cr.prueba.id = e.id " +
-                "WHERE comp.estadoParticipacion = 'Pendiente' AND c.id = :idCampeonato " +
+                "WHERE comp.estadoParticipacion = 'Confirmado' AND c.id = :idCampeonato " +
                 "ORDER BY e.nombre, u.sexo, comp.categoria";
 
         TypedQuery<CompetidorDetalleDTO> query = entityManager.createQuery(sql, CompetidorDetalleDTO.class);
